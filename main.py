370bd4c49adcfebdec8e56f41ed32bbd36aaa97d
mfavata <markfavata@gmail.com>
2026-01-07 20:06:17 -0500

    final bookbot project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_word_count, get_char_count
+from stats import get_word_count, get_char_count, sorted_dict
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -7,10 +8,21 @@ def get_book_text(file_path):
     return file_contents
 
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_text = get_book_text(sys.argv[1])
     num_words = get_word_count(book_text)
     characters = get_char_count(book_text)
-    print(f'Found {num_words} total words')
-    print(characters)
+    print(f'''============ BOOKBOT ============
+Analyzing book found at {sys.argv[1]}...
+----------- Word Count ----------
+Found {num_words} total words
+--------- Character Count -------''')
+    char_dict=sorted_dict(characters)
+    for ch in char_dict:
+        print(f'{ch["char"]}: {ch["num"]}')
+    print('============= END ===============')
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,6 @@
+def sort_on(items):
+    return items["num"]
+
 def get_word_count(file_contents):
     words = file_contents.split()
     return len(words)
@@ -13,3 +16,13 @@ def get_char_count(file_contents):
             char_dict[file_contents[i]] = 1
 
     return char_dict
+
+def sorted_dict(characters):
+    sorted_chars = []
+    for char in characters:
+        if char.isalpha():
+            char_dict = {"char": char, "num": characters[char],}
+            sorted_chars.append(char_dict)
+    
+    sorted_chars.sort(reverse=True, key=sort_on)
+    return sorted_chars
